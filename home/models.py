ebf95e610a7f51d7ca6bbe08c9b1964ae5b72d0a
dex <tianya3232@126.com>
2025-12-16 15:06:15 +0800

    Fix corrupted home/models.py

--- home/models.py
+++ home/models.py
@@ -1,8 +1,99 @@
+from django.db import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.images.models import Image
+from modelcluster.fields import ParentalKey
+from wagtail.models import Orderable
 
-# ... (rest of imports are same, skipping lines)
 
-# ... (existing classes: HomePageBanner, HomePageServiceItem, HomePageTeamMember, HomePage)
+class HomePageBanner(Orderable):
+    """轮播图"""
+    page = ParentalKey('home.HomePage', on_delete=models.CASCADE, related_name='banners')
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    title = models.CharField(max_length=255, blank=True)
+    subtitle = models.CharField(max_length=255, blank=True)
+    link = models.URLField(blank=True)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('title'),
+        FieldPanel('subtitle'),
+        FieldPanel('link'),
+    ]
+
+
+class HomePageServiceItem(Orderable):
+    """业务领域项目"""
+    page = ParentalKey('home.HomePage', on_delete=models.CASCADE, related_name='service_items')
+    title = models.CharField(max_length=100)
+    icon_red = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    icon_white = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    link = models.URLField(blank=True, default='/yewulingyu/')
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('icon_red'),
+        FieldPanel('icon_white'),
+        FieldPanel('link'),
+    ]
+
+
+class HomePageTeamMember(Orderable):
+    """首页团队成员"""
+    page = ParentalKey('home.HomePage', on_delete=models.CASCADE, related_name='team_members')
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=200, blank=True)
+    photo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    link = models.URLField(blank=True, default='/zhuanyetuandui/')
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('position'),
+        FieldPanel('photo'),
+        FieldPanel('link'),
+    ]
+
+
+class HomePage(Page):
+    # 关于我们简介
+    about_intro = RichTextField(blank=True, verbose_name="关于我们简介")
+    
+    # 主律师信息
+    main_lawyer_name = models.CharField(max_length=100, blank=True, default='吴少博')
+    main_lawyer_position = models.CharField(max_length=200, blank=True, default='北京吴少博律师事务所创始人、主任')
+    main_lawyer_photo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    main_lawyer_link = models.URLField(blank=True, default='/zhuanyetuandui/')
 
     content_panels = Page.content_panels + [
         InlinePanel('banners', label="轮播图"),
